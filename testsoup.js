50610936a12ca9d52df13b7a462d1015c77dc375
Giuseppe Martino <martinogiuseppe@gmail.com>
2013-07-19 13:51:55 +0200

    not important

--- testsoup.js
+++ testsoup.js
@@ -8,10 +8,9 @@ var message = Soup.Message.new('GET', "http://edos.debian.net/edos-debcheck/resu
 
 _httpSession.send_message(message);
 
-print(message.response_body.data);
 let xml = XML(message.response_body.data);
 
-weather_pool = { '1': 'clear',
+let weather_pool = { '1': 'clear',
 		 '2': 'few_clouds',
 		 '3': 'overcast',
 		 '4': 'shower',
